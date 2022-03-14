428793421292e73e7c38770c3456cbb5d65698a3
Tegah Badru <j.badru@outlook.com>
2022-03-14 12:28:55 +0000

    Updated ZAP API library fixes file

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
-echo $dockerImageName
+# dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
+# echo $dockerImageName
+echo $imageName
 
 docker run --rm -v "{$WORKSPACE}":/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName
 docker run --rm -v "{$WORKSPACE}":/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
